83c4b4ebb6ebff43689331959f82ab58d50b299c
Fede_Fiore <77865732+ref1o@users.noreply.github.com>
2023-12-22 15:21:34 +0100

    api-handler setupped and liverness function implemented

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -6,9 +6,40 @@ import (
 
 // Handler returns an instance of httprouter.Router that handle APIs registered here
 func (rt *_router) Handler() http.Handler {
-	// Register routes
-	rt.router.GET("/", rt.getHelloWorld)
-	rt.router.GET("/context", rt.wrap(rt.getContextReply))
+
+	// login
+	rt.router.POST("/session", rt.wrap(rt.sessionHandler))
+
+	// search
+	rt.router.GET("/users", rt.wrap(rt.getUsersQuery))
+
+	// user
+	rt.router.PUT("/users/:id", rt.wrap(rt.putNickname))
+	rt.router.GET("/users/:id", rt.wrap(rt.getUserProfile))
+
+	// ban
+	rt.router.PUT("/users/:id/banned_users/:banned_id", rt.wrap(rt.putBan))
+	rt.router.DELETE("/users/:id/banned_users/:banned_id", rt.wrap(rt.deleteBan))
+
+	// followers
+	rt.router.PUT("/users/:id/followers/:follower_id", rt.wrap(rt.putFollow))
+	rt.router.DELETE("/users/:id/followers/:follower_id", rt.wrap(rt.deleteFollow))
+
+	// stream
+	rt.router.GET("/users/:id/home", rt.wrap(rt.getHome))
+
+	// photo
+	rt.router.POST("/users/:id/photos", rt.wrap(rt.postPhoto))
+	rt.router.DELETE("/users/:id/photos/:photo_id", rt.wrap(rt.deletePhoto))
+	rt.router.GET("/users/:id/photos/:photo_id", rt.wrap(rt.getPhoto))
+
+	// comments
+	rt.router.POST("/users/:id/photos/:photo_id/comments", rt.wrap(rt.postComment))
+	rt.router.DELETE("/users/:id/photos/:photo_id/comments/:comment_id", rt.wrap(rt.deleteComment))
+
+	// likes
+	rt.router.PUT("/users/:id/photos/:photo_id/likes/:like_id", rt.wrap(rt.putLike))
+	rt.router.DELETE("/users/:id/photos/:photo_id/likes/:like_id", rt.wrap(rt.deleteLike))
 
 	// Special routes
 	rt.router.GET("/liveness", rt.liveness)
